bb838a40d2390e667f330547eb8f8afc0f67debc
CostaMAyssa <mayssafccn16@gmail.com>
2025-01-30 12:44:54 -0300

    icone

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:device_preview/device_preview.dart';
 import 'package:mundo_verde_game/screens/game/home_page.dart';
 import 'package:mundo_verde_game/screens/game/mission_selection_screen.dart';
 import 'package:mundo_verde_game/screens/game/game_story_screen.dart';
@@ -9,12 +8,7 @@ import 'screens/menu/profile_screen.dart';
 import 'screens/menu/rewards_screen.dart';
 
 void main() {
-  runApp(
-    DevicePreview(
-      enabled: true, // Ativa o Device Preview
-      builder: (context) => MyApp(), // App principal
-    ),
-  );
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,20 +16,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'GameTask',
-      useInheritedMediaQuery: true, // Necessário para Device Preview
       debugShowCheckedModeBanner: false,
       initialRoute: '/',
       routes: {
         '/': (context) => LoginScreen(),
-       '/story': (context) => GameStoryScreen(),
-       '/avatar': (context) => MissionSelectionScreen(),
+        '/story': (context) => GameStoryScreen(),
+        '/avatar': (context) => MissionSelectionScreen(),
         '/game': (context) => HomePage(),
         '/missions': (context) => MissionsScreen(),
         '/profile': (context) => ProfileScreen(),
         '/rewards': (context) => RewardsScreen(),
       },
-      // Adiciona a integração com o Device Preview
-      builder: DevicePreview.appBuilder,
     );
   }
 }
